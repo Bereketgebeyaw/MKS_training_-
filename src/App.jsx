17edba9733ec39b5b56726b633a8423e4b0e0ff0
Bereketgebeyaw <101499035+Bereketgebeyaw@users.noreply.github.com>
2024-08-26 00:00:29 +0300

    Complete the banner by adding the button

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Header from "./components/Header/Header"
+import Banner from './components/Banner';
 export default function App(){
   return (
     <>
       <Header/>
+      <Banner />
     </>
-  );in
+  );
 }
---
+++ src/components/Banner.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import Button from './Button/Button';
+
+export default function Banner() {
+  return (
+    <div
+      style={{
+        textAlign: 'center',
+        position: 'relative',
+        borderRadius: '6px',
+        overflow: 'hidden',
+        margin: '0 auto',
+        width: '1473px', // Set the width of the banner
+        height: '454px', // Set the height of the banner
+      }}
+    >
+      <img
+        src="/content1.png"
+        alt="Banner Image"
+        style={{
+          width: '100%',
+          height: '100%',
+          objectFit: 'cover',
+        }}
+      />
+      <div
+        style={{
+          position: 'absolute',
+          bottom: 0,
+          left: '50px',
+          
+          
+          padding: '20px',
+          boxSizing: 'border-box',
+        }}
+      >
+        <Button
+          props={{
+            color: '#ffcc00',
+            padding: '15px 30px',
+            textColor: '#fff',
+            title: 'REGISTER NOW',
+          }}
+        />
+      </div>
+    </div>
+  );
+}
